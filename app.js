53db6e92f8fc1440444a304ed7f3c7aed0ce099d
valeria-lm <vplopezm@gmail.com>
2023-05-29 03:09:31 -0300

    add new changes - skeleton loader animation

---
+++ app.js
@@ -0,0 +1,7 @@
+let loader = document.getElementById('loader');
+let spacePage = document.getElementById('spacePage');
+
+setTimeout(function() {
+  loader.style.display = 'none';
+  spacePage.style.display = 'block';
+}, 5000);
